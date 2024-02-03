5c7f2271b4edf6f7fa8fee2d497062b251373394
orange-spring <sporange@qq.com>
2024-02-03 12:54:41 +0800

    [修正] 添加网站图标, 补充参考资料, 修正部分CSS细节

--- 抄一个Google主页/main.js
+++ 抄一个Google主页/main.js
@@ -17,7 +17,6 @@ SearchText.onkeydown = function() {
     }
 }
 
-//功能实现部分参考资料:
-https://blog.csdn.net/Xmumu_/article/details/119807027 ;
-https://developer.mozilla.org/zh-CN/docs/Learn/JavaScript/Building_blocks/Events#%E4%BA%8B%E4%BB%B6%E5%AF%B9%E8%B1%A1 ;
-https://blog.csdn.net/qq_43968080/article/details/103186302 ;
+// 功能实现部分参考资料:
+// https://blog.csdn.net/Xmumu_/article/details/119807027
+// https://developer.mozilla.org/zh-CN/docs/Learn/JavaScript/Building_blocks/Events#%E4%BA%8B%E4%BB%B6%E5%AF%B9%E8%B1%A1
